d4eccb9f15869ed49d507b994139187423317484
saba-fatima-dev <sabafatima@metavystic.com>
2025-06-17 16:50:26 +0500

    added map export feature

--- src/MapView.tsx
+++ src/MapView.tsx
@@ -2,9 +2,11 @@
 import React from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
 import MapScaleControl from "./components/MapScaleControl";
+import MapExportControl from "./components/MapExportControl";
 
 const MapView: React.FC = () => {
   return (
+    <>
     <MapContainer
       center={[33.6844, 73.0479]}
       zoom={15}
@@ -17,6 +19,8 @@ const MapView: React.FC = () => {
       />
       <MapScaleControl />
     </MapContainer>
+    <MapExportControl />
+    </>
   );
 };
 
